bb6a9412ba26ccd2de8ab625c4e8d00a1c33e34a
Iara Pedrosa <iaraferreirahg@gmail.com>
2022-03-29 12:27:38 -0300

    primeiro requisito

---
+++ src/context/StarwarsProvider.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import StarwarsContext from './StarwarsContext';
+
+function StarwarsProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  const contextData = {
+    data,
+    setData,
+  };
+
+  return (
+    <StarwarsContext.Provider value={ contextData }>
+      {children}
+    </StarwarsContext.Provider>
+  );
+}
+
+StarwarsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default StarwarsProvider;
