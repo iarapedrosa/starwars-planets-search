fd2a264dd602e4bc10d7ccda79aab645319830c1
Iara Pedrosa <iaraferreirahg@gmail.com>
2022-09-03 21:35:57 -0300

    Adiciona imagem de topo e background
    Inicia estilização com css, adicionando imagens

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import FiltersHeader from './components/FiltersHeader';
 import Table from './components/Table';
 import StarwarsProvider from './context/StarwarsProvider';
+import topo from './images/grafismo-topo.svg';
 
 function App() {
   return (
-    <StarwarsProvider>
-      <FiltersHeader />
-      <Table />
-    </StarwarsProvider>
+    <section className="background">
+      <div className="grafismo">
+        <img src={ topo } alt="grafismo do topo" />
+      </div>
+      <StarwarsProvider>
+        <FiltersHeader />
+        <Table />
+      </StarwarsProvider>
+    </section>
   );
 }
 
